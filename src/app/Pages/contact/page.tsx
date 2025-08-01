16eee701d79979de0a5d34134f3536924a00fdd1
Teachz77 <teachhatz96@gmail.com>
2025-08-01 11:47:50 +0700

    little change

--- src/app/Pages/contact/page.tsx
+++ src/app/Pages/contact/page.tsx
@@ -23,7 +23,7 @@ export default function ContactPage() {
 
             {/* Gmail */}
             <div className="flex flex-row items-center gap-3">
-              <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg width="40" height="40" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" fill="#343C54"/>
               </svg>
               <a href="mailto:matthew.manuels17@gmail.com" className="underline">matthew.manuels17@gmail.com</a>
